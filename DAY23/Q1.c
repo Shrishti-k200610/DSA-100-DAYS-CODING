587fd0dbf9a6d3d8effc9df92cdbfb506055c9df
Shrishti-k200610 <shrishti.24983@stu.upes.ac.in>
2026-02-23 22:17:34 +0530

    Q1

--- DAY23/Q1.c
+++ DAY23/Q1.c
@@ -6,30 +6,41 @@ struct Node {
     struct Node* next;
 };
 
-// create node
-struct Node* newNode(int x) {
-    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-    temp->data = x;
-    temp->next = NULL;
-    return temp;
+// Function to create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
-// merge two sorted lists
-struct Node* merge(struct Node* a, struct Node* b) {
-    if (a == NULL) return b;
-    if (b == NULL) return a;
+// Merge two sorted lists
+struct Node* merge(struct Node* l1, struct Node* l2) {
+    struct Node dummy;          // temporary node
+    struct Node* tail = &dummy;
+    dummy.next = NULL;
 
-    if (a->data < b->data) {
-        a->next = merge(a->next, b);
-        return a;
-    } else {
-        b->next = merge(a, b->next);
-        return b;
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data < l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
     }
+
+    if (l1 != NULL)
+        tail->next = l1;
+    else
+        tail->next = l2;
+
+    return dummy.next;
 }
 
-// print list
-void print(struct Node* head) {
+// Print list
+void printList(struct Node* head) {
     while (head != NULL) {
         printf("%d ", head->data);
         head = head->next;
@@ -38,28 +49,34 @@ void print(struct Node* head) {
 
 int main() {
     int n, m, x;
-    struct Node *head1 = NULL, *head2 = NULL, *temp;
+    struct Node *l1 = NULL, *l2 = NULL, *temp, *head1 = NULL, *head2 = NULL;
 
-    // first list
+    // First list
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &x);
-        temp = newNode(x);
-        temp->next = head1;
-        head1 = temp;
+        temp = createNode(x);
+        if (head1 == NULL)
+            head1 = temp;
+        else
+            l1->next = temp;
+        l1 = temp;
     }
 
-    // second list
+    // Second list
     scanf("%d", &m);
     for (int i = 0; i < m; i++) {
         scanf("%d", &x);
-        temp = newNode(x);
-        temp->next = head2;
-        head2 = temp;
+        temp = createNode(x);
+        if (head2 == NULL)
+            head2 = temp;
+        else
+            l2->next = temp;
+        l2 = temp;
     }
 
-    head1 = merge(head1, head2);
-    print(head1);
+    struct Node* result = merge(head1, head2);
+    printList(result);
 
     return 0;
 }
