922f9f982872e5fc2a0d7bd0b12d6f21272579e5
Shrishti-k200610 <shrishti.24983@stu.upes.ac.in>
2026-03-01 22:20:58 +0530

    Q1

--- DAY29/Q1.c
+++ DAY29/Q1.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Node {
     int data;
     struct Node* next;
 };
-
 // Function to create new node
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -13,7 +11,6 @@ struct Node* createNode(int data) {
     newNode->next = NULL;
     return newNode;
 }
-
 // Function to rotate list right by k places
 struct Node* rotateRight(struct Node* head, int k) {
     if (head == NULL || head->next == NULL || k == 0)
@@ -27,38 +24,29 @@ struct Node* rotateRight(struct Node* head, int k) {
         temp = temp->next;
         length++;
     }
-
     // Make list circular
     temp->next = head;
-
     // Reduce k if greater than length
     k = k % length;
-
     // Find new tail position (length - k)
     int steps = length - k;
     temp = head;
-
     for (int i = 1; i < steps; i++) {
         temp = temp->next;
     }
-
     // Set new head
     struct Node* newHead = temp->next;
     temp->next = NULL;
-
     return newHead;
 }
 
 int main() {
     int n, k, value;
     struct Node *head = NULL, *temp = NULL;
-
     scanf("%d", &n);
-
     for (int i = 0; i < n; i++) {
         scanf("%d", &value);
         struct Node* newNode = createNode(value);
-
         if (head == NULL) {
             head = newNode;
             temp = head;
@@ -67,17 +55,13 @@ int main() {
             temp = newNode;
         }
     }
-
     scanf("%d", &k);
-
     head = rotateRight(head, k);
-
     // Print rotated list
     temp = head;
     while (temp != NULL) {
         printf("%d ", temp->data);
         temp = temp->next;
     }
-
     return 0;
 }
