0efcc81d76ff77638a1ff43c588bdea1a2a5a6bf
Shrishti-k200610 <shrishti.24983@stu.upes.ac.in>
2026-04-18 23:28:19 +0530

    Q1.c

---
+++ DAY77/Q1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int adj[MAX][MAX];
+int visited[MAX];
+int n, m;
+
+void dfs(int node) {
+    visited[node] = 1;
+    
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    // Initialize adjacency matrix and visited array
+    for (int i = 1; i <= n; i++) {
+        visited[i] = 0;
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // Undirected graph
+    }
+
+    // Run DFS from node 1
+    dfs(1);
+
+    // Check if all nodes are visited
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED\n");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED\n");
+    return 0;
+}
